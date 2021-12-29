28effe32598ca04e6f06e5faf2a7a15dc1abe55e
Hasnae <hasnae@viqueen.org>
2021-12-29 16:04:53 +1100

    welcome atlaskit renderer

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import Page, { Grid, GridColumn } from '@atlaskit/page';
-import { fontFamily } from '@atlaskit/theme';
+import sampleDocument from '../assets/sample-atlassian-document.json';
+import { ReactRenderer } from '@atlaskit/renderer';
 
 export default function Content() {
     return (
         <div>
             <Page>
                 <Grid>
-                    <GridColumn medium={8}>
-                        <h1>Main heading</h1>
-                        <p>Left content</p>
-                    </GridColumn>
-                    <GridColumn medium={4}>
-                        <h2>Sidebar</h2>
-                        <p>Right content</p>
-                    </GridColumn>
                     <GridColumn>
-                        <h2>Content below which takes up remaining space</h2>
-                        <p>The whole page content</p>
+                        <ReactRenderer document={sampleDocument} />
                     </GridColumn>
                 </Grid>
             </Page>
