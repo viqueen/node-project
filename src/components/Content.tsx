89fe7c4e33660f1922712e3d941ee8193882f787
Hasnae <hasnae@viqueen.org>
2021-12-30 17:37:10 +1100

    welcome heading component, also bring in some real life content so I can tame that content renderer

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Page, { Grid, GridColumn } from '@atlaskit/page';
-import sampleDocument from '../assets/sample-atlassian-document.json';
 import { ReactRenderer } from '@atlaskit/renderer';
+
+import nodeProjectContent from '../assets/node-project-content.json';
+import Heading from '@atlaskit/heading';
 
 export default function Content() {
     return (
-        <div>
+        <div style={{ marginTop: 60 }}>
             <Page>
                 <Grid>
                     <GridColumn>
-                        <ReactRenderer document={sampleDocument} />
+                        <div style={{ marginBottom: 40 }}>
+                            <Heading level="h800">
+                                {nodeProjectContent.title}
+                            </Heading>
+                        </div>
+                        <ReactRenderer document={nodeProjectContent.document} />
                     </GridColumn>
                 </Grid>
             </Page>
