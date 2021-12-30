139014b62b78990e05081092f20d4083632cff93
Hasnae <hasnae@viqueen.org>
2021-12-30 18:01:04 +1100

    welcome react-intl for i18n handling, just wrapping an IntlProvider around the app for now

--- src/components/Application.tsx
+++ src/components/Application.tsx
@@ -2,34 +2,31 @@ import React from 'react';
 import Navigation from './Navigation';
 import Content from './Content';
 import { fontFamily } from '@atlaskit/theme';
-import {
-    HashRouter,
-    Redirect,
-    Route,
-    Switch,
-    useParams
-} from 'react-router-dom';
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
 
 export default function Application() {
     return (
-        <HashRouter>
-        <div
-            style={{
-                top: 0,
-                position: 'fixed',
-                left: 0,
-                width: '100%',
-                fontFamily: fontFamily()
-            }}
-        >
-            <Navigation />
-            <Switch>
-                <Redirect exact from="/" to="/home" />
-                <Route path="/home">
-                    <Content />
-                </Route>
-            </Switch>
-        </div>
-        </HashRouter>
+        <IntlProvider locale="en">
+            <HashRouter>
+                <div
+                    style={{
+                        top: 0,
+                        position: 'fixed',
+                        left: 0,
+                        width: '100%',
+                        fontFamily: fontFamily()
+                    }}
+                >
+                    <Navigation />
+                    <Switch>
+                        <Redirect exact from="/" to="/home" />
+                        <Route path="/home">
+                            <Content />
+                        </Route>
+                    </Switch>
+                </div>
+            </HashRouter>
+        </IntlProvider>
     );
 }
